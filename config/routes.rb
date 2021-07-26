f1ccca329442ee1dfdab85f89c5c6cd8f5ead9dd
Ramiro Calderon <ramirocalderon@oxschool.edu.mx>
2021-07-26 03:23:26 -0500

    9.0

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   resources :student
   resources :drop
   resources :list
-  get "/drop/index.html", to: "drop#index"
-  get "/group/:id", to: "group#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
